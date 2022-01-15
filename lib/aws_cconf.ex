c7d75eba86a85a1069827dd77c4e9c9d30eb8735
kjetil midtlie <kjetil.midtlie@gmail.com>
2022-01-16 00:13:52 +0100

    9 green tests. source_profile recursive expander

--- lib/aws_cconf.ex
+++ lib/aws_cconf.ex
@@ -4,14 +4,16 @@ defmodule AwsCconf do
 
   """
 
-  @creds_kv_subset ["aws_access_key_id", "aws_secret_access_key", "aws_session_token"]
-  # OVERLOOK other kv's
+  @creds_attrs_subset [
+    "aws_access_key_id",
+    "aws_secret_access_key",
+    "aws_session_token"
+  ]
 
   # TRIGGERS for new "STS Session credentials"
-  # TODO structs?
-  # @attr_src_p "role_arn"
-  # @attr_mfa "mfa_serial"
-  # @attr_sso_url "sso_start_url"
+  @attr_src_p "source_profile"
+  @attr_mfa "mfa_serial"
+  @attr_sso_url "sso_start_url"
 
   @doc """
   Parse & merge config streams.
@@ -23,51 +25,51 @@ defmodule AwsCconf do
   """
   @spec combine(String.t(), {map, map}, bool) :: map
   def combine(
-        profile_name \\ "default",
-        {credentials, config},
-        strict_cred_field_selection? \\ true
+        pname \\ "default",
+        {creds, config},
+        strict_creds_subset? \\ true,
+        recurse_src_profile_attr? \\ true
       ) do
-    p_creds =
-      credentials
-      |> get_credentials_p(profile_name)
-      # Strict according to aws doc
-      |> then(
-        &if strict_cred_field_selection? do
-          Map.take(&1, @creds_kv_subset)
-        else
-          &1
-        end
-      )
+    attrs_list =
+      case [creds[pname], get_config_p(config, pname)] do
+        [nil, conf_p] ->
+          [conf_p]
 
-    p_conf = get_config_p(config, profile_name)
-    merge_profile_kvs([p_creds, p_conf])
+        [creds_p, conf_p] when strict_creds_subset? ->
+          [Map.take(creds_p, @creds_attrs_subset), conf_p]
+
+        [creds_p, conf_p] ->
+          [creds_p, conf_p]
+      end
+
+    attrs = merge_profile_attrs(attrs_list)
+
+    case attrs do
+      %{@attr_src_p => src_p} when recurse_src_profile_attr? ->
+        %{
+          attrs
+          | @attr_src_p => combine(src_p, {creds, config}, strict_creds_subset?, false)
+        }
+
+      _ ->
+        attrs
+    end
   end
 
-  # FIXME all 3 could be :error !!
-  defp get_credentials_p(profiles, profile) do
-    profiles[profile]
-    # TODO Map.filter(@creds_kv_subset)
+  defp get_config_p(profiles, pname) when pname == "default" do
+    profiles[pname]
   end
 
-  defp get_config_p(profiles, profile) when profile == "default" do
-    profiles[profile]
-  end
-
-  defp get_config_p(profiles, profile) do
+  defp get_config_p(profiles, pname) do
     profiles
     |> Map.keys()
     # TODO compile regex to avoid special-char injection
-    |> Enum.find(&String.match?(&1, ~r/profile\ *#{profile}/))
+    |> Enum.find(&String.match?(&1, ~r/profile\ *#{pname}/))
     |> then(&profiles[&1])
   end
 
-  # FIXME both could be nil !!
-  def merge_profile_kvs(profile_maps) do
+  defp merge_profile_attrs(profile_maps) do
     profile_maps
     |> Enum.reduce(fn x, acc -> Enum.into(acc, x) end)
   end
-
-  # def extract_kvs(profiles, kv_subset \\ []) do
-  #   1
-  # end
 end
--- test/aws_cconf_test.exs
+++ test/aws_cconf_test.exs
@@ -26,10 +26,20 @@ defmodule AwsCconfTest do
         "profile other" => %{
           "region" => "eu-west-1"
         },
+        "profile accesskeys" => %{
+          "aws_access_key_id" => "in_conf",
+          "aws_secret_access_key" => "will_work_also",
+          "cli_pager" => "more"
+        },
         "profile trigger" => %{
           "region" => "eu-west-1",
           "role_arn" => "aws:arn:dada",
           "source_profile" => "other"
+        },
+        "profile another_trigger" => %{
+          "region" => "eu-north-1",
+          "role_arn" => "aws:arn:-----",
+          "source_profile" => "accesskeys"
         }
       }
     ]
@@ -74,11 +84,27 @@ defmodule AwsCconfTest do
 
     result2 = AwsCconf.combine("default", {ctx.test_creds, ctx.test_conf}, false)
     assert result2["aws_access_key_id"] == "ABC"
+    assert result2["cli_pager"] == "less"
   end
 
-  test "role_arn value will require a source_profile", ctx do
+  test "'source_profile' as be recursive map attr (default)", ctx do
     result = AwsCconf.combine("trigger", {ctx.test_creds, ctx.test_conf})
-    IO.inspect(result)
-    assert result["source_profile"] == "other"
+    # IO.inspect(result)
+    assert %{
+             "aws_access_key_id" => "abc"
+           } = result["source_profile"]
+
+    result2 = AwsCconf.combine("another_trigger", {ctx.test_creds, ctx.test_conf})
+    # IO.inspect(result)
+    assert %{
+             "aws_access_key_id" => "in_conf",
+             "cli_pager" => "more"
+           } = result2["source_profile"]
+  end
+
+  test "'source_profile' as normal string attr", ctx do
+    result = AwsCconf.combine("trigger", {ctx.test_creds, ctx.test_conf}, true, false)
+    # IO.inspect(result)
+    assert "other" = result["source_profile"]
   end
 end
